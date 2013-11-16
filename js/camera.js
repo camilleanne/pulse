b75fdf878c9c4078e4d4ff4ddb033f47ee672a66
Camille Teicheira <cteicheira@gmail.com>
2013-11-15 16:21:13 -0800

    removed old comments and questions now that those issues have been solved

--- js/camera.js
+++ js/camera.js
@@ -2,12 +2,11 @@ var camera = (function(){
   var htracker;
   var video, canvas, context, videoPause, canvasOverlay, overlayContext;
   var countdownCanvas, countdownContext, hrCanvas, hrContext;
-  var renderTImer;
+  var renderTimer;
   var width = 480;
   var height = 360;
   var fps = 15;
   var greenTimeSeries = [];
-  var dataWindow = false;
   var heartrate;
   var heartrateArray = [];
   var lastTime;
@@ -43,7 +42,6 @@ var camera = (function(){
     canvas = document.createElement("canvas");
     canvas.setAttribute('width', width);
     canvas.setAttribute('height', height);
-    // canvas.style.top = '0px'
     context = canvas.getContext("2d");
 
     canvasOverlay = document.createElement("canvas");
@@ -105,8 +103,7 @@ var camera = (function(){
     sw = (event.width/2.5) >> 0;
     sh = (event.height/4) >> 0;
 
-    // console.log(sx, sy, sw, sh)
-
+    //** for testing without headtracking
     // sx = sy = 200;
     // sw = sh = 50;
 
@@ -134,32 +131,15 @@ var camera = (function(){
       // overlayContext.fillStyle = "rgba(200, 0, 0, 1)";
       // overlayContext.fillRect(sx, sy, sw, sh);
       
-      // Notes: forehead video box --
-      // current theory is that the video brings in images at ~130dpi (@ 320 x 240), 
-      // whereas the canvas is 72dpi, thus the .545 or 1.85 difference
-      // this isn't perfect, something about the way it calculates, 
-      // there is still an offset image sometimes. 
-      // Constraints will allow a specific resolution (theoretically), will require testing.
-      // This currently only works for the iSight in a MacBook Air
-    
-      // ** for 320 x 240
-      // overlayContext.drawImage(video, event.x + (sx* .8334), sy * 1.8334, sw*1.8334, sh*1.8334, sx, sy, sw, sh);
-
-      // ** for 480 x 320          
-      // overlayContext.drawImage(video, (sx * 1.33), sy, sw * 1.3333, sh * 1.3333, sx, sy, sw, sh);
-
-      // ** for 640 x 480 (which is too big for what I want do be doing, but brings the video down to 72dpi)
-      // overlayContext.drawImage(video, sx, sy - 10, sw, sh, sx, sy, sw, sh);
-
       //turn green
       forehead = context.getImageData(sx, sy, sw, sh);
-
       for (i = 0; i < forehead.data.length; i+=4){
         //for reference:
         // var red = forehead.data[i];
         // var green = forehead.data[i+1];
         // var blue = forehead.data[i+2];
         // var alpha = forehead.data[i+3];
+        // for putting a green video image on screen
         // forehead.data[i] = 0;
         // forehead.data[i+2] = 0;
 
@@ -169,11 +149,8 @@ var camera = (function(){
 
       //get average of green area for each frame
       var average = greenSum/forehead.data.length;
-      // console.log("Average: ", average);
 
       //create an array of averages for last 30 seconds
-      //normalize
-      // cardiac();
       if (greenTimeSeries.length < bufferWindow){
         greenTimeSeries.push(average);
         
@@ -184,14 +161,11 @@ var camera = (function(){
         countdownContext.fillText(((bufferWindow - greenTimeSeries.length)/fps) >> 0, 25, 25)
         countdownContext.restore()
 
-        // console.log('countdown: ', bufferWindow - greenTimeSeries.length);
       } else {
-        // dataWindow = true;
         greenTimeSeries.push(average);
         greenTimeSeries.shift();
 
         cardiac();
-        // heartbeatTimer();
 
 
       }
@@ -212,22 +186,22 @@ var camera = (function(){
 
 
   function cardiac(){
+    // static test fft spectrum data:
     // var fftArray = [0, 0.6082253456115723, 0.25601473450660706, 0.3510543704032898, 0.33191803097724915, 0.40043285489082336, 0.24442104995250702, 0.2906903624534607, 0.10941631346940994, 0.22052565217018127, 0.08654522150754929, 0.11624716222286224, 0.17962567508220673, 0.10583585500717163, 0.14719387888908386, 0.1313147246837616, 0.06234072521328926, 0.1543150544166565, 0.11760584264993668, 0.11862088739871979, 0.12146361917257309, 0.266795814037323, 0.21472491323947906, 0.06073959171772003, 0.06288193166255951, 0.09347641468048096, 0.036240000277757645, 0.05102884769439697, 0.04235406965017319, 0.02831820584833622, 0.05210259556770325, 0.10648924112319946, 0.15421639382839203, 0.06438228487968445, 0.10806077718734741, 0.03795776888728142, 0.08309036493301392, 0.031337834894657135, 0.09866298735141754, 0.014706060290336609, 0.03916962072253227, 0.08954672515392303, 0.029007576406002045, 0.057937536388635635, 0.008652705699205399, 0.037266805768013, 0.02773413620889187, 0.06986536830663681, 0.003169334027916193, 0.11411335319280624, 0.08028649538755417, 0.023082412779331207, 0.07458089292049408, 0.035777997225522995, 0.03824623301625252, 0.049640655517578125, 0.077757328748703, 0.034727200865745544, 0.04745856299996376, 0.023465096950531006, 0.024489138275384903, 0.07100922614336014, 0.040082309395074844, 0.05462462082505226, 0.018537191674113274, 0.022122405469417572, 0.07421161979436874, 0.030828898772597313, 0.015992337837815285, 0.041366539895534515, 0.022134358063340187, 0.04682140797376633, 0.04059939831495285, 0.03882011026144028, 0.03683715686202049, 0.0355040617287159, 0.04130915179848671, 0.0772508755326271, 0.017217449843883514, 0.04906228929758072, 0.05653959885239601, 0.03592155873775482, 0.05430029705166817, 0.08023825287818909, 0.021106261759996414, 0.015047682449221611, 0.025425495579838753, 0.01588442362844944, 0.010336599312722683, 0.04358170926570892, 0.03919610008597374, 0.059951890259981155, 0.0548163503408432, 0.0708637610077858, 0.05006611347198486, 0.0253529604524374, 0.024189390242099762, 0.04341737926006317, 0.021617475897073746, 0.023999590426683426, 0.03697226196527481, 0.04166150838136673, 0.048380929976701736, 0.023121561855077744, 0.028621751815080643, 0.02181928977370262, 0.06232449412345886, 0.003735166508704424, 0.056197550147771835, 0.007498697843402624, 0.026589369401335716, 0.036785390228033066, 0.06052883714437485, 0.02696179784834385, 0.06407647579908371, 0.07175770401954651, 0.04474520683288574, 0.05206694453954697, 0.0502486452460289, 0.04056066647171974, 0.04000169411301613, 0.029190203174948692, 0.0447375625371933, 0.05608018860220909, 0.04813564941287041, 0.04150288179516792, 0.03023460879921913, 0.013040808029472828]
 
     var normalized = normalize(greenTimeSeries);
-
 
     // fast fourier transform from dsp.js
     var fft = new RFFT(bufferWindow, fps);
     fft.forward(normalized);
     var spectrum = fft.spectrum;
-    console.log(spectrum.length)
 
     var freq = frequencyExtract(spectrum, fps);
     // console.log("peak freq in hz:", freq)
 
     heartrate = freq * 60;
     // console.log("heartrate: ", heartrate)
+
     heartrateArray.push(heartrate)
 
     //draw heartbeat to page
@@ -251,16 +225,6 @@ var camera = (function(){
 
   };
 
-//**
-  // function heartbeatTimer(){
-  //       // while (dataWindow == true){
-  //   var calculationTimer = setInterval(function(){
-  //       cardiac();
-  //     }, 1000);
-
-  //   // };
-
-  // }
   function pauseCapture(){
     if (renderTimer) clearInterval(renderTimer);
     // if (calculationTimer) clearInterval(calculationTimer);
