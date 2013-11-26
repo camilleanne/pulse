0a8ba93e99bb10ef62efb050af6843fca4f964d5
Camille Teicheira <cteicheira@gmail.com>
2013-11-25 22:04:24 -0800

    added visualizations of data

--- static/js/camera.js
+++ static/js/camera.js
@@ -3,8 +3,8 @@ var camera = (function(){
   var video, canvas, context, videoPause, canvasOverlay, overlayContext;
   var countdownCanvas, countdownContext, hrCanvas, hrContext;
   var renderTimer;
-  var width = 480;
-  var height = 360;
+  var width = 640;
+  var height = 480;
   var fps = 15;
   var heartrate;
   var heartrateArray = [];
@@ -12,11 +12,14 @@ var camera = (function(){
   var bufferWindow = 1024;
   var workingBuffer;
   var countdown = false;
-  // var rgbMatrix = [[],[],[]];
   var red = [];
   var green = [];
   var blue = [];
+  // var time = [];
   var pause = false;
+  var spectrum;
+  // var confidenceGraph;
+  var confidenceGraph, x, y, line;
 
   function initVideoStream(){
     video = document.createElement("video");
@@ -68,7 +71,6 @@ var camera = (function(){
     countdownDiv.appendChild(countdownCanvas);
 
     startCapture();
-
   };
 
   function headtrack (){      
@@ -78,8 +80,6 @@ var camera = (function(){
 
     // for each facetracking event received draw rectangle around tracked face on canvas
     document.addEventListener("facetrackingEvent", greenRect)
-
-
   };
 
   function greenRect(event) {
@@ -90,25 +90,24 @@ var camera = (function(){
     var greenSum = 0;
     var redSum = 0;
     var blueSum = 0;
-    var normalized = [];
     
     //approximating forehead based on facetracking
     sx = event.x + (-(event.width/5)) >> 0;
     sy = event.y + (-(event.height/3)) >> 0;
-    sw = (event.width/2.5) >> 0;
-    sh = (event.height/4) >> 0;
+    sw = (event.width/4) >> 0;
+    sh = (event.height/6) >> 0;
+    // sw = (event.width/3) >> 0;
+    // sh = (event.height/5) >> 0;
 
     // CS == camshift (in headtrackr.js)
     // once we have stable tracking, draw rectangle
     if (event.detection == "CS") /**/ {
-      
       // Notes: 
       // translate moves the origin point of context by event.x and event.y
       // ex. (88, 120) becomes the new (0, 0), removing translating for 
       // the moment and adding (event.x + or event.y + ) where needed for now
 
       // overlayContext.translate(event.x, event.y)
-
       overlayContext.rotate(event.angle-(Math.PI/2));
       overlayContext.strokeStyle = "#00CC00";
 
@@ -117,82 +116,39 @@ var camera = (function(){
       // blue forehead box (for debugging)
       overlayContext.strokeStyle = "#33CCFF";       
       overlayContext.strokeRect(sx, sy, sw, sh);
+
+      forehead = context.getImageData(sx, sy, sw, sh);
       
       //turn green
-      forehead = context.getImageData(sx, sy, sw, sh);
       for (i = 0; i < forehead.data.length; i+=4){
         //for reference:
         // var red = forehead.data[i];
         // var green = forehead.data[i+1];
         // var blue = forehead.data[i+2];
         // var alpha = forehead.data[i+3];
-        
+
         // for putting a green video image on screen
-        // forehead.data[i] = 0;
-        // forehead.data[i+2] = 0;
+        // forehead.data[i] = forehead.data[i];
+        // forehead.data[i + 1] = forehead.data[i]
+        // forehead.data[i + 2] = forehead.data[i];
+
 
         //get sum of green area for each frame
         redSum = forehead.data[i] + redSum;
         greenSum = forehead.data[i+1] + greenSum;
         blueSum = forehead.data[i+2] + blueSum;
       };
-
+      
       //get average of green area for each frame
-      var average = greenSum/forehead.data.length;
-      var redAverage = redSum/forehead.data.length;
-      var blueAverage = blueSum/forehead.data.length;
+      var redAverage = redSum/(forehead.data.length/4);
+      var greenAverage = greenSum/(forehead.data.length/4);
+      var blueAverage = blueSum/(forehead.data.length/4);
 
-      //cascade so user doesn't have to wait 60sec for a heartbeat, but allows for greater accuracy over time
-      if (red.length < (bufferWindow/8)){
-        red.push(redAverage);
-        green.push(average);
-        blue.push(blueAverage);
-
-        countdown = true;
-
-      } else if (red.length < (bufferWindow/4)){
-        workingBuffer = bufferWindow/8
-        red.push(redAverage);
-        green.push(average);
-        blue.push(blueAverage);
-        sendData(JSON.stringify({"array": [red.slice(red.length - workingBuffer), green.slice(green.length - workingBuffer), blue.slice(blue.length - workingBuffer)], "bufferWindow": workingBuffer}));
-        console.log('128')
-
-      } else if (red.length < (bufferWindow/2)){
-        workingBuffer = bufferWindow/4
-        red.push(redAverage);
-        green.push(average);
-        blue.push(blueAverage);
-        sendData(JSON.stringify({"array": [red.slice(red.length - workingBuffer), green.slice(green.length - workingBuffer), blue.slice(blue.length - workingBuffer)], "bufferWindow": workingBuffer}));
-        console.log('256')
-
-      } else if (red.length < bufferWindow) {
-        workingBuffer = bufferWindow/2
-        red.push(redAverage);
-        green.push(average);
-        blue.push(blueAverage);
-        sendData(JSON.stringify({"array": [red.slice(red.length - workingBuffer), green.slice(green.length - workingBuffer), blue.slice(blue.length - workingBuffer)], "bufferWindow": workingBuffer}));
-        console.log('512')
-
-      } else {
-        workingBuffer = bufferWindow
-        red.push(redAverage);
-        red.shift();
-
-        green.push(average);
-        green.shift();
-
-        blue.push(blueAverage);
-        blue.shift();
-
-        countdown = false;
-        sendData(JSON.stringify({array:[red, green, blue], bufferWindow: bufferWindow}));
-      }
-
-
+      shiftDataWindow(redAverage, greenAverage, blueAverage);
+      
       // for putting green video image on screen
-      // overlayContext.putImageData(forehead, sx, sy);
-      overlayContext.rotate((Math.PI/2)-event.angle); //**
+      // overlayContext.putImageData(forehead, event.x + (-(event.width/2)) >> 0, event.y + (-(event.height/2)) >> 0);
+      overlayContext.rotate((Math.PI/2)-event.angle);
       
       // see note above about .translate()
       // overlayContext.translate(-event.x, -event.y);
@@ -206,6 +162,61 @@ var camera = (function(){
 
   };
 
+
+  function shiftDataWindow(redAverage, greenAverage, blueAverage){
+    // cascade so user doesn't have to wait 60sec for a heartbeat
+    // allows for greater accuracy over time
+    if (red.length < (bufferWindow/8)){
+        red.push(redAverage);
+        green.push(greenAverage);
+        blue.push(blueAverage);
+        // time.push(Date.now());
+        countdown = true;
+        // console.log(time)
+
+      } else if (red.length < (bufferWindow/4)){
+        workingBuffer = bufferWindow/8
+        red.push(redAverage);
+        green.push(greenAverage);
+        blue.push(blueAverage);
+        // time.push(Date.now());
+        sendData(JSON.stringify({"array": [red.slice(red.length - workingBuffer), green.slice(green.length - workingBuffer), blue.slice(blue.length - workingBuffer)], "bufferWindow": workingBuffer, })); //"time": time.slice(time.length - workingBuffer)
+
+      } else if (red.length < (bufferWindow/2)){
+        workingBuffer = bufferWindow/4
+        red.push(redAverage);
+        green.push(greenAverage);
+        blue.push(blueAverage);
+        // time.push(Date.now());
+        sendData(JSON.stringify({"array": [red.slice(red.length - workingBuffer), green.slice(green.length - workingBuffer), blue.slice(blue.length - workingBuffer)], "bufferWindow": workingBuffer, })); //"time":time.slice(time.length - workingBuffer)
+
+      } else if (red.length < bufferWindow) {
+        workingBuffer = bufferWindow/2
+        red.push(redAverage);
+        green.push(greenAverage);
+        blue.push(blueAverage);
+        // time.push(Date.now());
+        sendData(JSON.stringify({"array": [red.slice(red.length - workingBuffer), green.slice(green.length - workingBuffer), blue.slice(blue.length - workingBuffer)], "bufferWindow": workingBuffer, })); //"time":time.slice(time.length - workingBuffer)
+
+      } else {
+        workingBuffer = bufferWindow
+        red.push(redAverage);
+        red.shift();
+
+        green.push(greenAverage);
+        green.shift();
+
+        blue.push(blueAverage);
+        blue.shift();
+
+        // time.push(Date.now());
+        // time.shift();
+        countdown = false;
+        sendData(JSON.stringify({"array":[red, green, blue], "bufferWindow": bufferWindow, })); //"time":time
+      }
+  }
+
+
   function drawCountdown(array){
     countdownContext.font = "20pt Helvetica";
     countdownContext.clearRect(0,0,200,100);
@@ -217,13 +228,17 @@ var camera = (function(){
 
   function cardiac(averagePixelArray, bfwindow){
     var normalized = normalize(averagePixelArray);
-
+    console.log(normalized)
+    // var normalized = averagePixelArray;
     // fast fourier transform from dsp.js
+    
     var fft = new RFFT(bfwindow, fps);
     fft.forward(normalized);
-    var spectrum = fft.spectrum;
+    spectrum = fft.spectrum;
+    // spectrum = averagePixelArray;
 
-    var freq = frequencyExtract(spectrum, fps);
+    var freqs = frequencyExtract(spectrum, fps);
+    var freq = freqs.freq_in_hertz
     heartrate = freq * 60;
     heartrateArray.push(heartrate)
 
@@ -234,10 +249,57 @@ var camera = (function(){
     countdownContext.fillText(heartrate >> 0, 25, 25);
     countdownContext.restore();
 
-    // console.log(normalized)
+    graphData = {one: normalized[normalized.length-1]}
+    // graphData.two =
+    // graphData = {one: (freqs.normalizedFreqs[freqs.normalizedFreqs.length-1] * 10)}
+    graph.series.addData(graphData);
+    graph.render();
+    showConfidenceGraph(freqs, 600, 100)
 
   };
   
+  var graph = new Rickshaw.Graph( {
+      element: document.getElementById("graph"),
+      width: 200,
+      height: 100,
+      renderer: 'line',
+      min: -5,
+      interpolation: 'basis',
+      series: new Rickshaw.Series.FixedDuration([{ name: 'one' }], undefined, {
+        timeInterval: 1000/fps,
+        maxDataPoints: 100,
+        timeBase: new Date().getTime() / 1000
+      })
+    });
+  function showConfidenceGraph(data, width, height){
+
+    var data = _.zip(data.normalizedFreqs, data.filteredFreqBin)
+    if (confidenceGraph){
+      if (data.length == 22){
+        y = d3.scale.linear().domain([0, 0.825]).range([height, 10]);
+      } else if (data.length == 44){
+        y = d3.scale.linear().domain([0, 0.77]).range([height, 30]);
+      } else if (data.length > 44){
+        y = d3.scale.linear().domain([0, 0.70]).range([height, 30]);
+      } 
+      confidenceGraph.selectAll("path").transition().attr("d", line(data)).attr("class", "line").ease("linear").duration(66);
+
+    } else {
+      confidenceGraph = d3.select("#confidenceGraph").append("svg").attr("width", width).attr("height", height);
+      x = d3.scale.linear().domain([0.75,4]).range([0, width]);
+      y = d3.scale.linear().domain([0, 1]).range([height, 0]);
+
+      line = d3.svg.line()
+        .x(function(d) { return x(+d[1]); })
+        .y(function(d) { return y(+d[0]); });
+
+      confidenceGraph.append("svg:path").attr("d", line(data)).attr("class", "line");
+    }
+
+
+  }
+
+  // }
   function startCapture(){
     video.play();
     pause = false;
